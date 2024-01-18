76149d2e4169b84897fc615aece8351afc0113bb
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-01-18 11:36:14 -0800

    class stopping point

--- server.js
+++ server.js
@@ -1,36 +1,63 @@
 const express = require('express');
-const app = express();
 const path = require('path')
+const fs = require('fs');
 const PORT = 3333;
 
+const app = express();
 
-// GET Route -- listen for localhost:3333/test
+app.use(express.json());
 
-app.get('/test', (requestObj, responseObj) => {
-    responseObj.send('<h1>Hi from the server</h1>');
+
+
+async function getUserData(){
+    const users = await fs.promises.readFile('./data.json', 'utf8');
+    return JSON.parse(users);
+}
+
+async function saveUserData(usersArr) {
+    await fs.promises.writeFile('./data.json', JSON.stringify(usersArr, null, 2))
+}
+
+
+
+// route to retrieve/get all users from json database
+
+app.get('/api/users', async (req, res) => {
+    // read data.json 
+    // const users = await fs.promises.readFile('./data.json', 'utf8');
+
+    const users = await getUserData();
+
+
+    res.send(users);
+
 })
 
-app.get('/', (req, res) => {
-    console.log(req);
-    res.send('U r in root')
-})
+// Route to add user to json db
+app.post('/api/users', async (req, res) => {
 
-app.get('/api/recipe', (req, res) => {
+    const users = await getUserData();
+
+
+    if(!users.find(user => user.username === req.body.username)){
+        users.push(req.body);
+
+        await saveUserData(users);
+        return res.send({message: 'User added successfully!'});
+    }
+
+    // await saveUserData(users);
+    // console.log('data recieved');
+
     res.send({
-        name:'cheese',
-        ingredients:['cheese', 'time']
+        error:402,
+        message: 'user already exists'
     })
+    
 })
 
-app.get('/page', (req, res) => {
-    // const path = 'C:\\Users\\johnn\\Desktop\\bootcamp\\practice\\express_overview\\index.html'
-    // console.log(path);
-    res.sendFile(path.join(__dirname, './index.html'));
-})
+// CRUD Creating, reading, u, deleting
 
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname, './notfound.html'))
-})
 
 app.listen(PORT, () => {
     console.log('Server started on port', PORT);
