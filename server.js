507c0b393adeceb393f365dfd9220e00af872c83
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-01-19 08:50:00 -0800

    css

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ async function saveUserData(usersArr) {
     await fs.promises.writeFile('./data.json', JSON.stringify(usersArr, null, 2))
 }
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
+app.get('/css/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/css/styles.css'));
+})
 
 // route to retrieve/get all users from json database
 
