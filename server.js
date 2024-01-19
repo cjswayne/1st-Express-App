9e3f6ca7d0d730f9c0bd01bce714596963b68b54
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-01-19 09:20:33 -0800

    added static files and cors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path')
 const fs = require('fs');
 const PORT = 3333;
 
+const cors = require('cors');
+
 const app = express();
 
 app.use(express.json());
@@ -18,13 +20,17 @@ async function saveUserData(usersArr) {
     await fs.promises.writeFile('./data.json', JSON.stringify(usersArr, null, 2))
 }
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
 
-app.get('/css/styles.css', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/css/styles.css'));
-})
+app.use(express.static('./public'));
+app.use(cors())
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// })
+
+// app.get('/css/styles.css', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/css/styles.css'));
+// })
 
 // route to retrieve/get all users from json database
 
